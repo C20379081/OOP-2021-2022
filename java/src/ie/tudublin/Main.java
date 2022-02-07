f580362ea23663230d567ea9bbcbb3820d579927
c20379081 <c20379081@mytudublin.ie>
2022-02-07 12:28:18 +0000

    init commit :zz :wq i :q
    §

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -7,6 +7,11 @@ public class Main
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
+    public static void BugZap()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new BugZap());
+    }
 
     public void cat()
     {
@@ -33,7 +38,7 @@ public class Main
     }
     public static void main(String[] args)
     {
-        helloProcessing();
+        BugZap();
 
         // Tara Misty
         // Tara Tara
